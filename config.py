c8d312543607e68bd5090e96208106a3ec57f62c
jpallard <jp@jpallard.com>
2019-11-25 12:14:13 -0500

    Pagination

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['jp@jpallard.com']
+    POSTS_PER_PAGE = 3
